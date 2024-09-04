15d636ecf9b13c2a89219c63b90a0de78ba388aa
Will-Montero <monteromora99@gmail.com>
2024-09-04 17:03:48 -0500

    card cmponent

--- src/components/CardProductos.jsx
+++ src/components/CardProductos.jsx
@@ -1,6 +1,50 @@
+import { useState } from "react";
+import '../styles/cardProductos.css'
 
-export const CardProductos = () => {
+export const CardProductos = ({
+  titulo,
+  imagen,
+  precio,
+  categoria,
+  handleAgregar,
+  handleEliminar,
+}) => {
+  const [añadir, setAñadir] = useState(false);
+
+  const clickAñadir = () => {
+    setAñadir(true);
+    handleAgregar()
+  };
+
+  const clickEliminar = () => {
+    setAñadir(false);
+    handleEliminar()
+  };
   return (
-    <div>CardProductos</div>
+    <>
+    <div className="tarjeta">
+      <img src={imagen} alt={titulo} className="tarjeta-imagen" />
+
+      <div className="tarjeta-contenido">
+        <h3 className="imagen-titulo">{titulo}</h3>
+        <p className="imagen-descripcion">{categoria}</p>
+        <p className="imagen-precio">{precio}</p>
+      </div>
+
+      {añadir
+      ?
+      <button
+      onClick={clickEliminar}
+      className="btn btn-danger"
+      type="button"
+      > Eliminar del carrito</button>
+      :
+      <button
+      onClick={clickAñadir}
+      className="btn btn-primary"
+      type="button"
+      >Agregar a Carrito</button>}
+    </div>
+    </>
   )
-}
+};
