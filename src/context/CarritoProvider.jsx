57b04dcc687c9078b27716c8a4d04d3cb265ed21
Will-Montero <monteromora99@gmail.com>
2024-09-04 16:04:06 -0500

    carrito provider

--- src/context/CarritoProvider.jsx
+++ src/context/CarritoProvider.jsx
@@ -1,6 +1,75 @@
+import { useReducer } from 'react'
+import { CarritoContext } from './CarritoContext'
 
-export const CarritoProvider = () => {
+
+const estadoIncial = []
+
+const productosReducer = async (state = estadoIncial, action = {}) => {
+    switch (action.value) {
+        case '[COMPRA]: Agregar compra':
+            return [...state, action.payload]
+        case '[COMPRA]: Aumentar compra':
+            return state.map( item => 
+            {
+                const cant = item.cantidad + 1;
+                if(item.id === action.payload) return {...item, cantidad: cant}
+                return item
+            } 
+            )
+        case '[COMPRA]: Disminuir compra':
+            return state.map( item => 
+                {
+                    const cant = item.cantidad - 1;
+                    if(item.id === action.payloadn && item.cantidad > 1) return {...item, cantidad: cant}
+                    return item
+                } 
+                )
+        case '[COMPRA]: Eliminar compra':
+            return state.filter(compra => compra.id !== action.payload ) 
+
+    
+        default:
+            return state
+    }
+}
+
+export const CarritoProvider = ({ children }) => {
+
+    const [stateProductos, dispatch] = useReducer(productosReducer, estadoIncial)
+
+    const AgregarCompra = (compra) => {
+        const action = {
+            type: '[COMPRA]: Agregar compra',
+            payload: compra
+        }
+        dispatch(action)
+    }
+    const AumentarCompra = (id) => {
+        const action = {
+            type: '[COMPRA]: Aumentar compra',
+            payload: id
+        }
+        dispatch(action)
+    }
+    const DisminuirCompra = (id) => {
+        const action = {
+            type: '[COMPRA]: Disminuir compra',
+            payload: id
+        }
+        dispatch(action)
+    }
+    const EliminarCompra = (id) => {
+        const action = {
+            type: '[COMPRA]: Eliminar compra',
+            payload: id
+        }
+        dispatch(action)
+    }
+
+
   return (
-    <div>CarritoProvider</div>
+    <CarritoContext.Provider value={{stateProductos, AumentarCompra, DisminuirCompra, EliminarCompra}}>
+        { children }
+    </CarritoContext.Provider>
   )
 }
