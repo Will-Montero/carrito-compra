f56693a0021674c093423a446667dd85271519c5
Will-Montero <monteromora99@gmail.com>
2024-09-04 16:58:03 -0500

    Productos page

--- src/pages/ProductosPage.jsx
+++ src/pages/ProductosPage.jsx
@@ -1,6 +1,37 @@
-
+import {CardProductos } from '../components/CardProductos'
+import { ProductosContext } from '../context/ProductosContext'
+import { CarritoContext } from '../context/CarritoContext'
+import { useContext } from 'react'
 export const ProductosPage = () => {
+
+ const { productos } = useContext(ProductosContext)
+ const { AgregarCompra, EliminarCompra } = useContext(CardProductos)
+
+ const handleAgregar = (compra) => {
+  AgregarCompra(compra)
+ }
+ const handleEliminar = (id) => {
+  EliminarCompra(id)
+ }
+
+
+
   return (
-    <div>ProductosPage</div>
+   <>
+   <h1>Productos</h1>
+   {productos.map(producto => {
+    return (
+      <CardProductos
+      key={producto.id}
+      titulo={producto.title}
+      precio={producto.price}
+      catregoria={producto.category}
+      imagen={producto.image}
+      handleAgregar={() => handleAgregar(producto)}
+      handleEliminar={() => handleEliminar(producto.id)}
+      ></CardProductos>
+    )
+   })}
+   </>
   )
 }
