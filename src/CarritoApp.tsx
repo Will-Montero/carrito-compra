c83008d59d42c8c12b61ee362c59f2b69f59386c
Will-Montero <monteromora99@gmail.com>
2024-10-22 18:06:00 -0500

    tsx

--- src/CarritoApp.tsx
+++ src/CarritoApp.tsx
@@ -1,23 +1,24 @@
-import { NavBar } from "./components/NavBar"
-import { CarritoProvider } from "./context/CarritoProvider"
-import { ProductosProvider } from './context/ProductosProvider'
-import { Routes, Route, Navigate } from "react-router-dom"
-import { ProductosPage } from "./pages/ProductosPage"
-import { CarritoProductos } from "./pages/CarritoProductos"
-import { Footer } from "./components/Footer"
+import { NavBar } from "./components/NavBar";
+import { CarritoProvider } from "./context/CarritoProvider";
+import { ProductosProvider } from './context/ProductosProvider';
+import { Routes, Route, Navigate } from "react-router-dom";
+import { ProductosPage } from "./pages/ProductosPage";
+import { CarritoProductos } from "./pages/CarritoProductos";
+import { Footer } from "./components/Footer";
 
-export const CarritoApp = () => {
+// Definimos que CarritoApp es un componente funcional de React
+export const CarritoApp: React.FC = () => {
   return (
     <ProductosProvider>
       <CarritoProvider>
-      <NavBar></NavBar>
-      <Routes>
-      <Route path="/" element={<ProductosPage/>}></Route>
-      <Route path="/carrito" element={<CarritoProductos/>}></Route>
-      <Route path="/*" element={<Navigate to='/'/>}></Route>
-      </Routes>
-      <Footer/>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<ProductosPage />} />
+          <Route path="/carrito" element={<CarritoProductos />} />
+          <Route path="/*" element={<Navigate to="/" />} />
+        </Routes>
+        <Footer />
       </CarritoProvider>
     </ProductosProvider>
-  )
-}
+  );
+};
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import { CarritoApp } from './CarritoApp'
+import React, { StrictMode } from 'react'; // Importamos React y StrictMode
+import { createRoot } from 'react-dom/client'; // Importamos createRoot desde react-dom
+import { BrowserRouter } from 'react-router-dom'; // Importamos BrowserRouter para manejar rutas
+import { CarritoApp } from './CarritoApp'; // Importamos el componente CarritoApp
 
+// Verificamos que el elemento con id 'root' exista y lo usamos para renderizar la aplicación
+const rootElement = document.getElementById('root') as HTMLElement; 
 
-createRoot(document.getElementById('root')).render(
+// Creamos la raíz de la aplicación y renderizamos el árbol de componentes
+createRoot(rootElement).render(
   <StrictMode>
- <BrowserRouter>
- <CarritoApp/>
- </BrowserRouter>
+    <BrowserRouter>
+      <CarritoApp />
+    </BrowserRouter>
   </StrictMode>,
-)
+);
